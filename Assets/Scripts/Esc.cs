34855f1ad4bbd25d28a369e3bfd0f43020248950
LAPTOP-I2ODA1HE\wb136 <1367574446@qq.com>
2025-06-06 20:28:12 +0800

    no message

---
+++ Assets/Scripts/Esc.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esc : MonoBehaviour
+{
+    public GameObject EditPanel;
+    private bool isOpen=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&&!isOpen)
+        {
+            
+            EditPanel.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && isOpen)
+        {
+
+            EditPanel.SetActive(false);
+        }
+    }
+}
